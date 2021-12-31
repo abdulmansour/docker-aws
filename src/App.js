7b2df80d6953de3e508cb369bd4a548425de9e2b
Abdulrahim Mansour <molate1591@gmail.com>
2021-12-31 11:58:51 -0600

    added profile links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ function App() {
   return (
     <div>
       <h1>Abdulrahim Mansour</h1>
+      <ul>
+        <li><a href="https://github.com/abdulmansour">GitHub</a></li>
+        <li><a href="https://www.linkedin.com/in/mansourabdulrahim">LinkedIn</a></li>
+      </ul>
     </div>
   );
 }
